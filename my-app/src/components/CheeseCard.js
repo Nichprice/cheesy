6db6397145d94aa73b62cfaf39de66cd56b1ff8b
nichprice <price.nich@gmail.com>
2022-06-19 13:26:01 -0400

    built cheese card, header, and some changes to cheesepage and app

--- my-app/src/components/CheeseCard.js
+++ my-app/src/components/CheeseCard.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function CheeseCard() {
-
+    return (
+        <div className="whole-card">
+            <div  className="name">
+                {cheese.name}
+            </div>
+            <div className="firmness">
+                {cheese.firmness}
+            </div>
+            <div className="image">
+                {cheese.image}
+            </div>
+            <div className="card-footer">
+                <button 
+                    className="info-button"
+                    onClick={}>
+                    Info
+                </button>
+                <button 
+                    className="addtocart-button">
+                    Add to 🛒
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default CheeseCard;
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,10 +5,17 @@ import cheeselogo from '../images/cheeselogo.png'
 function Header() {
  return (
   <div>
-    <img src={cheeselogo}/> 
+    <img 
+      className="cheesy-home-button" 
+      src={cheeselogo}
+      onClick
+    /> 
     <h1>
       cheesy
     </h1>
+    <button className="cart-button">
+      Your Cart
+    </button>
   </div>
  )
 }
