85c230e0f2b7e8451284da20752d332ee2bd3fc9
Danny Linhardt <daniel.linhardt@gmail.com>
2022-06-22 11:46:30 -0400

    remove from cart button added

--- my-app/src/components/CartCheese.js
+++ my-app/src/components/CartCheese.js
@@ -1,15 +1,15 @@
 import React from "react";
-import CheeseCard from "./CheeseCard";
+import CartCard from "./CartCard";
 import "../AllCheese.css";
 
 
 //showing as two your carts
-function CartCheese({cheeseCart, capitalize, renderInfo}){
+function CartCheese({cheeseCart, capitalize, renderInfo, removeFromCart}){
   return (
     <h2>Cheese Cart
       <div className="row">  
       {cheeseCart.map((cheese) => {
-        return <CheeseCard key={cheese.id} cheese={cheese} capitalize={capitalize} renderInfo={renderInfo}/>
+        return <CartCard key={cheese.id} cheese={cheese} capitalize={capitalize} renderInfo={renderInfo} removeFromCart={removeFromCart}/>
       })}
       </div>
     </h2>
